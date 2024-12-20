b45a4c80f22668b6e2ec519da981896f3ede6468
GANDURAZ <159551339+GANDURAZ@users.noreply.github.com>
2024-12-20 04:19:43 +0300

    Delete Rezka.py

--- Rezka.py
+++
@@ -1,7 +0,0 @@
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-
-name_file = "KRSK DS-I456Z 39_20241214-143245--20241214-143345.avi"
-start_time = 5
-end_time = 36
-
-ffmpeg_extract_subclip(name_file, start_time, end_time)
