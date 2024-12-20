b25ca036d12c788adf7ca8974b837cdc308cbf80
GANDURAZ <159551339+GANDURAZ@users.noreply.github.com>
2024-12-20 04:19:51 +0300

    Delete speed.py

--- speed.py
+++
@@ -1,8 +0,0 @@
-from moviepy.editor import *
-from moviepy.video.fx.all import speedx
-
-video = VideoFileClip("KRSK DS-I456Z 39_20241214-143245--20241214-143345SUB5000_30000.avi")
-
-# Ускорение видео в 2 раза
-video_fast = video.fx(speedx, 2)
-video_fast.write_videofile("fast_video.mp4")
